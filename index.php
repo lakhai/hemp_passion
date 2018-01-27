f016fcf5d33eaed1410ab75c1fc3ad740729c0f4
Sebastian Salines <salines.sebastian@gmail.com>
2018-01-27 02:04:01 +0000

    Created development branch, many changes and started translating to spanish:

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
  * @var bool
  */
 define('WP_USE_THEMES', true);
+@ini_set('upload_max_filesize', '12M');
+@ini_set('post_max_size','13M');
+@ini_set('memory_limit','15M');
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
