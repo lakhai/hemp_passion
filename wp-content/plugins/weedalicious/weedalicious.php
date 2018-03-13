83f478bea03201dcf0b2771a0dc4291b1855b377
Sebastian Salines <sebastian@capicua.com.uy>
2018-03-13 01:29:00 -0300

    trying stuf

--- wp-content/plugins/weedalicious/weedalicious.php
+++ wp-content/plugins/weedalicious/weedalicious.php
@@ -257,46 +257,46 @@ function getIndexes($row) {
     return $existing;
 }
 
-function clearWoocommerce() {
-    global $wpdb;
+// function clearWoocommerce() {
+//     global $wpdb;
 
-    //delete tags
-    $result[] = $wpdb->query("DELETE relations.*, taxes.*, terms.*
-    FROM wp_term_relationships AS relations
-    INNER JOIN wp_term_taxonomy AS taxes
-    ON relations.term_taxonomy_id=taxes.term_taxonomy_id
-    INNER JOIN wp_terms AS terms
-    ON taxes.term_id=terms.term_id
-    WHERE object_id IN (SELECT ID FROM wp_posts WHERE post_type='product');");
+//     //delete tags
+//     $result[] = $wpdb->query("DELETE relations.*, taxes.*, terms.*
+//     FROM wp_term_relationships AS relations
+//     INNER JOIN wp_term_taxonomy AS taxes
+//     ON relations.term_taxonomy_id=taxes.term_taxonomy_id
+//     INNER JOIN wp_terms AS terms
+//     ON taxes.term_id=terms.term_id
+//     WHERE object_id IN (SELECT ID FROM wp_posts WHERE post_type='product');");
     
-    $result[] = $wpdb->query("DELETE FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type = 'product');");
-    $result[] = $wpdb->query("DELETE FROM wp_posts WHERE post_type = 'product';");
+//     $result[] = $wpdb->query("DELETE FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type = 'product');");
+//     $result[] = $wpdb->query("DELETE FROM wp_posts WHERE post_type = 'product';");
     
-    $result[] = $wpdb->query("DELETE pm FROM wp_postmeta pm LEFT JOIN wp_posts wp ON wp.ID = pm.post_id WHERE wp.ID IS NULL;");
+//     $result[] = $wpdb->query("DELETE pm FROM wp_postmeta pm LEFT JOIN wp_posts wp ON wp.ID = pm.post_id WHERE wp.ID IS NULL;");
     
-    $result[] = $wpdb->query("delete from `wp_termmeta`
-    where 
-        `term_id` in ( 
-            SELECT `term_id`
-            FROM `wp_term_taxonomy`
-            WHERE `taxonomy` in ('product_cat', 'product_type', 'product_visibility') 
-        );");
+//     $result[] = $wpdb->query("delete from `wp_termmeta`
+//     where 
+//         `term_id` in ( 
+//             SELECT `term_id`
+//             FROM `wp_term_taxonomy`
+//             WHERE `taxonomy` in ('product_cat', 'product_type', 'product_visibility') 
+//         );");
     
-    $result[] = $wpdb->query("delete from `wp_terms`  
-    where 
-        `term_id` in ( 
-            SELECT `term_id`
-            FROM `wp_term_taxonomy`
-            WHERE `taxonomy` in ('product_cat', 'product_type', 'product_visibility') 
-        );");
+//     $result[] = $wpdb->query("delete from `wp_terms`  
+//     where 
+//         `term_id` in ( 
+//             SELECT `term_id`
+//             FROM `wp_term_taxonomy`
+//             WHERE `taxonomy` in ('product_cat', 'product_type', 'product_visibility') 
+//         );");
         
-    $result[] = $wpdb->query("DELETE FROM `wp_term_taxonomy` WHERE `taxonomy` in ('product_cat', 'product_type', 'product_visibility');");
+//     $result[] = $wpdb->query("DELETE FROM `wp_term_taxonomy` WHERE `taxonomy` in ('product_cat', 'product_type', 'product_visibility');");
 
-    $result[] = $wpdb->query("DELETE meta FROM wp_termmeta meta LEFT JOIN wp_terms terms ON terms.term_id = meta.term_id WHERE terms.term_id IS NULL;");
+//     $result[] = $wpdb->query("DELETE meta FROM wp_termmeta meta LEFT JOIN wp_terms terms ON terms.term_id = meta.term_id WHERE terms.term_id IS NULL;");
 
-    $result[] = $wpdb->query("DELETE FROM wp_woocommerce_attribute_taxonomies;");
+//     $result[] = $wpdb->query("DELETE FROM wp_woocommerce_attribute_taxonomies;");
 
-    $result[] = $wpdb->query("DELETE FROM wp_woocommerce_sessions;");
+//     $result[] = $wpdb->query("DELETE FROM wp_woocommerce_sessions;");
     
-    return $result;
-}
+//     return $result;
+// }
